4fce8a1c8f2a0834647bb26c59b29a8e07a78676
Jack King <email@qiaohong.org>
2018-07-12 23:31:02 +0800

    modified test code

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from tornado.ioloop import IOLoop
+from tornado.httpclient import HTTPRequest, HTTPResponse
 from core.spider import BaseSpider
+from core.scheduler import QueueScheduler
+from core.downloader import AsyncDownloader
 
-try:
-    spider = BaseSpider()
+
+class MySpider(BaseSpider):
+    counter = 0
+
+    def start(self):
+        for _ in range(1000):
+            yield HTTPRequest("https://blog.qiaohong.org")
+
+    def parse(self, response: HTTPResponse):
+        self.counter += 1
+        print(self.counter, response.code, response.error)
+
+
+if __name__ == '__main__':
+    spider = MySpider(QueueScheduler(AsyncDownloader(), max_concurrency=32))
     spider.run()
-except KeyboardInterrupt:
-    IOLoop.current().stop()
