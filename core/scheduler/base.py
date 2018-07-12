9e288f1f4426fcac08a1381a217a86d3dc69e3f1
Jack King <email@qiaohong.org>
2018-07-12 11:34:25 +0800

    wrap basic ops to base scheduler

--- core/scheduler/base.py
+++ core/scheduler/base.py
@@ -1,4 +1,26 @@
+import functools
+from typing import Callable
 from tornado.httpclient import HTTPRequest
+from core.downloader.base import BaseDownloader
+from core.utils.call import call_increase, call_decrease
+
+
+def cb_decrease(value):
+    """
+    decrease concurrency when put func's callback called.
+    :return: decorator
+    """
+
+    def wrapper(func):
+        @functools.wraps(func)
+        def inner_wrapper(obj: BaseScheduler, request: HTTPRequest, callback: Callable):
+            callback = call_decrease(value)(callback)
+
+            return func(obj, request, callback)
+
+        return inner_wrapper
+
+    return wrapper
 
 
 class BaseScheduler(object):
@@ -6,17 +28,28 @@ class BaseScheduler(object):
     Interface for Scheduler class
     """
 
+    def __init__(self, downloader: BaseDownloader,
+                 max_concurrency: int = 64):
+        self.downloader = downloader
+        self.max_concurrency = max_concurrency
+        self.concurrency = 0
+
+        # increase concurrency when fetch func called
+        self.downloader.fetch = call_increase(self.concurrency)(self.downloader.fetch)
+
     def get(self) -> HTTPRequest:
         """
         get request item from scheduler
-        :return: HTTPRequest Object
+        :return: HTTPRequest Object or None
         """
         pass
 
-    def put(self, request: HTTPRequest) -> None:
+    @cb_decrease
+    def put(self, request: HTTPRequest, callback: Callable) -> None:
         """
         put request item from scheduler
         :param request:
+        :param callback: call back func
         :return: None
         """
         pass
--- core/scheduler/queue.py
+++ core/scheduler/queue.py
@@ -1,6 +1,9 @@
 from queue import Queue
+from typing import Callable
 from tornado.httpclient import HTTPRequest
-from .base import BaseScheduler
+from core.downloader.base import BaseDownloader
+from core.scheduler.base import BaseScheduler
+from core.utils.call import call_increase, call_decrease
 
 
 class QueueScheduler(BaseScheduler):
@@ -9,8 +12,12 @@ class QueueScheduler(BaseScheduler):
     Be careful of memory run out.
     """
 
-    def __init__(self):
+    def __init__(self, downloader: BaseDownloader,
+                 max_concurrency: int = 64):
+        super(QueueScheduler, self).__init__(downloader,
+                                             max_concurrency=max_concurrency)
         self.queue = Queue()
+        self.downloader.fetch = call_increase(self.concurrency)(self.downloader.fetch)
 
     def get(self):
         if not self.queue.empty():
@@ -18,8 +25,12 @@ class QueueScheduler(BaseScheduler):
         else:
             return None
 
-    def put(self, request: HTTPRequest):
-        self.queue.put_nowait(request)
+    def put(self, request: HTTPRequest, callback: Callable):
+
+        if self.concurrency < self.max_concurrency:
+            self.downloader.fetch(request, call_decrease(self.concurrency)(callback))
+        else:
+            self.queue.put_nowait((request, callback))
 
     def empty(self):
         return self.queue.empty()
