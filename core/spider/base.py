4f0012db4a07e91b215e6f8a92ebfaef9430df17
Jack King <email@qiaohong.org>
2018-07-12 22:40:39 +0800

    modified run func

--- core/spider/base.py
+++ core/spider/base.py
@@ -1,4 +1,5 @@
 from typing import Generator
+from tornado.ioloop import IOLoop
 from core.scheduler.base import BaseScheduler
 from core.downloader.base import BaseDownloader
 from tornado.httpclient import HTTPRequest, HTTPResponse
@@ -24,6 +25,7 @@ class BaseSpider(object):
     def parse(self, response: HTTPResponse) -> Generator:
         """
         Default parse function.
+        todo: how to handle this function's request objects
         :param response:
         :return:
         """
@@ -31,8 +33,8 @@ class BaseSpider(object):
 
     def run(self):
         """
-        Spider run method
-        :return:
+        run spider
+        :return: None
         """
 
         for item in self.start():
@@ -40,3 +42,9 @@ class BaseSpider(object):
             assert isinstance(item[0], HTTPRequest)
             req, cb = item if len(item) == 2 else item[0], self.parse
             self.scheduler.put(req, cb)
+
+        loop = IOLoop.current()
+        try:
+            loop.start()
+        except KeyboardInterrupt:
+            loop.stop()
