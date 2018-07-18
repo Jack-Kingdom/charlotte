0e94863647beef53d257433d17d7a614f3e20656
Jack King <email@qiaohong.org>
2018-07-18 18:15:00 +0800

    slightly modified downloader

--- charlotte/downloader/base.py
+++ charlotte/downloader/base.py
@@ -1,4 +1,4 @@
-from tornado.httpclient import HTTPRequest
+from tornado.httpclient import HTTPRequest, HTTPResponse
 
 
 class BaseDownloader(object):
@@ -6,6 +6,8 @@ class BaseDownloader(object):
     Interface for Downloader
     """
 
+    # downloader's middleware focused on patch request & response
+    # don't support filter request & response
     middleware = ()
 
     def fetch(self, request: HTTPRequest) -> None:
@@ -13,6 +15,14 @@ class BaseDownloader(object):
         Load middleware and fetch page.
         You need to implement fetch by extends this func.
         :param request: HTTPRequest object
+        :return: None
+        """
+        pass
+
+    def handle(self, response: HTTPResponse):
+        """
+        handle response and middleware, call callback func
+        :param response: HTTPResponse object
         :return: None
         """
         pass
