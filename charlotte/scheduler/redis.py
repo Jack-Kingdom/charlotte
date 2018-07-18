837ac60725d7f793f3b574da69f5c31775007db5
Jack King <email@qiaohong.org>
2018-07-18 18:17:24 +0800

    implement redis scheduler

--- charlotte/scheduler/redis.py
+++ charlotte/scheduler/redis.py
@@ -1,9 +1,34 @@
-from queue import Queue
+import pickle
+from redis import Redis
 from tornado.httpclient import HTTPRequest
+from ..downloader.base import BaseDownloader
 from .base import BaseScheduler
 
 
 class RedisScheduler(BaseScheduler):
     """
-    todo Redis Scheduler implement
+    In memory queue scheduler.
+    Be careful of memory run out.
     """
+
+    def __init__(self,
+                 redis: Redis = None,
+                 downloader: BaseDownloader = None):
+        super(RedisScheduler, self).__init__(downloader)
+        self.redis = redis
+
+    def get(self):
+        if not self.empty():
+            return pickle.loads(self.redis.lpop('queue'))
+        else:
+            return None
+
+    def put(self, request: HTTPRequest):
+
+        if self.concurrency < self.max_concurrency:
+            self.fetch(request)
+        else:
+            self.redis.lpush('queue', pickle.dumps(request))
+
+    def empty(self):
+        return not self.redis.llen('queue')
