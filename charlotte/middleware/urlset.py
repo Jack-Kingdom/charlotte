4c2c8c763619073bc6e27b9b424cab430628adca
Jack King <email@qiaohong.org>
2018-07-23 10:34:01 +0800

    replace request url with hexdigest add to set

--- charlotte/middleware/urlset.py
+++ charlotte/middleware/urlset.py
@@ -1,8 +1,20 @@
 import logging
+import hashlib
 from tornado.httpclient import HTTPRequest, HTTPResponse
 from .base import BaseMiddleWare
 
 logger = logging.getLogger(__name__)
+
+
+def to_hash(url: str) -> str:
+    """
+    hash url
+    :param url: url string
+    :return: hex digest
+    """
+    url = url.encode('utf-8')
+
+    return hashlib.sha256(url).hexdigest()
 
 
 class URLSetFilter(BaseMiddleWare):
@@ -15,16 +27,21 @@ class URLSetFilter(BaseMiddleWare):
 
     def handle_req(self, request: HTTPRequest):
 
-        if request.url in self.set:
+        digest = to_hash(request.url)
+
+        if digest in self.set:
             logger.debug('page {0} in set, filtered.'.format(request.url))
             return None
         else:
             logger.debug('page {0} add to set.'.format(request.url))
-            self.set.add(request.url)
+            self.set.add(digest)
             return request
 
     def handle_res(self, response: HTTPResponse):
+
+        digest = to_hash(response.request.url)
+
         if response.code == 599:
             logger.debug('page {0} fetch error, remove from set.'.format(response.request.url))
-            self.set.remove(response.request.url)
+            self.set.remove(digest)
         return response
