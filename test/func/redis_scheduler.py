f15392c3cce3e57a80f5f6bb27a4ed90538507c3
Jack King <email@qiaohong.org>
2018-07-23 11:46:43 +0800

    modified redis test script

--- test/func/redis_scheduler.py
+++ test/func/redis_scheduler.py
@@ -8,10 +8,9 @@ logger = logging.getLogger()
 logger.setLevel('DEBUG')
 logger.addHandler(logging.StreamHandler(sys.stdout))
 
-counter = 0
-
 
 class MySpider(BaseSpider):
+    counter = 0
     scheduler = RedisScheduler(redis=Redis(host='localhost', port=6379))
 
     def start(self):
@@ -19,9 +18,8 @@ class MySpider(BaseSpider):
             yield "https://blog.qiaohong.org"
 
     def parse(self, response):
-        global counter
-        counter += 1
-        print(counter, response.code, response.error)
+        self.counter += 1
+        print(self.counter, response.code, response.error)
 
 
 if __name__ == '__main__':
