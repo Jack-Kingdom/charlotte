bf2e9e08e5661b9cb6ef37d4b5664a5359f2a12d
Jack King <email@qiaohong.org>
2019-03-20 22:28:17 +0800

    updated version num

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='charlotte',
     packages=find_packages(exclude=('test', 'tests.*')),
-    version='0.1.1',
+    version='0.2.0',
     url='https://github.com/Jack-Kingdom/charlotte',
     description='Lightweight and expandable spider framework.',
     long_description=open('README.md').read(),
